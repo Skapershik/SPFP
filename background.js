38bbe94f95ee997aec782f62c46a02e7c33bff53
Skapershik <adsroslyakovsergey@yandex.ru>
2019-01-07 01:26:27 +0400

    Rework
    Удалил все лишнее, изменил архитектуру кода. На данный момент работают запросы по ссылкам на Danbooru и псевдо конструктор поста

--- background.js
+++ background.js
@@ -3,25 +3,91 @@
 // found in the LICENSE file.
 
 'use strict';
+function dataURItoBlob(dataURI) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    var byteString = atob(dataURI.split(',')[1]);
 
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        console.log("This is a first install!");
-        let ScriptSettings = {
-            EasyTags: true, //Разделение тегов запятыми для удобного копирования
-            Pixiv: true, //Формирование ссылки на Pixiv из названия файла
-            IQDB: true, //Отправка файла на IQDB, получение тегов с Danbooru
-            CallModerator: true, // Кнопка быстрого вызова администрации сообщества
-            //IQDBModes\\
-            BetaIQDB: true, //Пробный режим, вывод результатов поиска IQDB непосредственно на страницу создания поста
-            IQDBAuto: true, //Автоматический запрос тегов по первому совпадению на IQDB, может выдать ахинею
-            Youtube: true, //Youtube плеер в комментариях
-            Yukari:true
-        };
-        chrome.storage.local.set({"Settings":ScriptSettings});
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
-    }else if(details.reason == "update"){
-        console.log("update!");
-        chrome.storage.local.set({"NewVersion": true});
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
     }
-});
+
+    return new Blob([ab], {type: mimeString});
+
+
+}
+function get(url){
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: "get",
+            url: url,
+            anonymous: true,
+            success: function (response) {
+                resolve(response)
+            },
+            error: function (e) {
+                reject('Error',e)
+            }
+        });
+    })
+}
+function post(url, _data){
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            type: 'post',
+            url: url,
+            processData: false,
+            contentType: false,
+            data: _data,
+            success: function (response) {
+                resolve(response);
+            },
+            error: function (e) {
+                reject('Error',e)
+            }
+        });
+    })
+}
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        request = JSON.parse(request)
+        console.log(request)
+        if (request.type !== undefined) {
+            if (request['type'] === 'get') {
+                if(request['url'].includes('danbooru.donmai.us/posts/')){
+                    request['url']+='.json'
+                }
+                console.log(request['url'])
+                get(request['url']).then(
+                    resolve => {
+                        console.log('resolve:', resolve)
+                        sendResponse(resolve)
+                    },
+                    reject => {
+                        console.log('reject:', reject)
+                        sendResponse(reject)
+                    }
+                )
+            }
+            else if (request['type'] === 'post') {
+                post(request['url'], request['data']).then(
+                    resolve => {
+                        console.log('resolve:', resolve)
+                        sendResponse(resolve)
+                    },
+                    reject => {
+                        console.log('reject:', reject)
+                        sendResponse(reject)
+                    }
+                )
+            }
+        }
+        return true;
+    });
